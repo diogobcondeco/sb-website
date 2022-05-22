02ddddc01d10da986650ae9e88e6dd1f823b71a6
Diogo Condêço <diogothedev@gmail.com>
2022-05-22 17:08:45 +0100

    cards improvement

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import '../../App.scss';
+import image from '../../assets/images/banner.jpg';
 
 interface Props {
   title: string
   description: string[]
 }
 
-const Card = ({title, description}: Props) => {
+const Card = ({ title, description }: Props) => {
   return (
-    <div className='flex flex-col p-4 rounded-xl min-w-[33%] cardShadow bg-gray-100'>
-      <div className='text-center text-xl font-bold'>{title}</div>
-      <div className='m-4'>{description.map((item, key) => {
-        return <div key={key}>{item}</div>
-      })}</div>
+    <div className='flex flex-col rounded-xl cardShadow bg-gray-100'>
+      <img className='h-40 rounded-t-lg rounded-s-none' src={image} alt='placeholder' />
+      <div className='p-4'>
+        <div className='text-center text-xl font-bold underline'>{title}</div>
+        <div className='m-4 indent-4 leading-8 tracking-wide'>{description.map((item, key) => {
+          return <div key={key}>{item}</div>
+        })}</div>
+      </div>
     </div>
   )
 };
